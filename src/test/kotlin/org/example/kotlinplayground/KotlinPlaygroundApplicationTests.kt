5f5d84b6cef3d3ad598bbbb0292f19d4c27195a8
dev-chpark <xpmxf5@gmail.com>
2024-12-17 06:53:49 +0900

    🎉 init: Intial Commit

---
+++ src/test/kotlin/org/example/kotlinplayground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlinplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
